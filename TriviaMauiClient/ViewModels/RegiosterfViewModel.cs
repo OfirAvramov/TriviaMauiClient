afe0c23ebbac7852facbfa6d95298793288a289a
ofiravr06@gmail.com <ofiravr06@gmail.com>
2023-10-16 20:11:01 +0300

    AddingRegister

---
+++ TriviaMauiClient/ViewModels/RegiosterfViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaMauiClient.ViewModels
+{
+    public class RegiosterfViewModel
+    {
+        private string password;
+        public string Password { get; set; }
+        private string username;
+        public string Username { get; set; }
+
+    }
+}
